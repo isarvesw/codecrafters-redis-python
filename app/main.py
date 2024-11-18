9a245aa8308457483f5a04350dbfc84178f20b2d
Ilan Sarveswaran <ilan.sarveswaran@apple.com>
2024-11-17 21:57:16 -0800

    respond to pong - version 2

--- app/main.py
+++ app/main.py
@@ -6,15 +6,17 @@ def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
-    # Uncomment this to pass the first stage
-    
+    pong = "+PONG\r\n"
+
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     server_socket.accept() # wait for client
 
-    connection, _ = server_socket.accept()
-    time.sleep(60)
-    connection.sendall(b"+PONG\r\n")
-    time.sleep(60)
+    conn, addr = server_socket.accept()
+
+    with conn:
+        conn.recv(1024)
+        conn.send(pong.encode)
+
 
 if __name__ == "__main__":
     main()
