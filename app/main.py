467c360bde3c2ada1dcd9fa3f68db2b7f3392050
Ilan Sarveswaran <24445207+isarvesw@users.noreply.github.com>
2024-11-18 18:55:14 -0800

    respond to pong - version 3

--- app/main.py
+++ app/main.py
@@ -5,18 +5,12 @@ import time
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
-
-    pong = "+PONG\r\n"
-
+    
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    # server_socket.accept() # wait for client
 
-    conn, addr = server_socket.accept()
-
-    with conn:
-        conn.recv(1024)
-        conn.send(pong.encode)
-
+    connection, _ = server_socket.accept()
+    connection.sendall(b"+PONG\r\n")
 
 if __name__ == "__main__":
     main()
